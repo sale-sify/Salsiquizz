49b4ab743d90d8dba2a032775e53510539f2cd34
Romain Sales <sales_r@etna-alternance.net>
2026-02-06 16:09:50 +0100

    DB connection

--- Salsiquizz-Backend/routes/users.js
+++ Salsiquizz-Backend/routes/users.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const connectDB = require('../db');
+
+app.use(express.json());
+connectDB();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
